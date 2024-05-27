d6688baaf745fb1506f2a181b3348a7405a18f91
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2024-05-28 00:57:53 +0530

    feat:add animations with AOS library

--- Components/Helper/Hero.tsx
+++ Components/Helper/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <video src="/Images/hero1.mp4" autoPlay muted loop preload="metadata" className='w-[100%] h-[100%] object-cover'/>
       <div className='absolute z-[100] w-[100%] h-[100%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <div className="flex items-center justify-center flex-col w-[100%] h-[100%]">
-          <div>
+          <div data-aos= "fade-right">
             <h1 className='text-[25px] mb-[1rem] md:mb-[0] text-center md:text-[35px] lg:text-[45px] tracking-[0.2rem] text-white font-bold uppercase'>Let's Enjoy the journey</h1>
             <p className='md:text-[16px] text-center text-[18px] text-white font-normal [word-spacing:5px]'>Get the best prices on 2,000,000+ properties, worldwide</p>
           
--- Components/Helper/Hotels.tsx
+++ Components/Helper/Hotels.tsx
@@ -6,22 +6,22 @@ const Hotels = () => {
     <div className='pt-[5rem] bg-gray-200 pb-[4rem]'>
       <h1 className='heading'>Best Hotels</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center w-[80%] mx-auto mt-[4rem]'>
-        <div> 
+        <div data-aos ="fade-left"> 
             <HotelCards name="hotel Nextus" city="Sydney" price="$125" reviewNum='37' image='/Images/h1.png'/>
          </div>
-        <div> 
+        <div data-aos="zoom-in" daya-aos-delay = "300"> 
             <HotelCards name="hotel Reactive" city="Dhaka" price="$125" reviewNum='27' image='/Images/h2.png'/>
          </div>
-        <div> 
+        <div data-aos="fade-right" daya-aos-delay = "600"> 
             <HotelCards name="hotel TypeScript" city="Tokyo" price="$125" reviewNum='53' image='/Images/h3.png'/>
          </div>
-        <div> 
+        <div data-aos="fade-left" daya-aos-delay = "900"> 
             <HotelCards name="hotel Postgreys" city="London" price="$125" reviewNum='37' image='/Images/h4.png'/>
          </div>
-        <div> 
+        <div data-aos="zoom-in" daya-aos-delay = "1000"> 
             <HotelCards name="hotel Crimsus" city="Bankok" price="$125" reviewNum='17' image='/Images/h5.png'/>
          </div>
-        <div> 
+        <div data-aos="fade-right" daya-aos-delay = "1600"> 
             <HotelCards name="hotel Thai" city="Texas" price="$725" reviewNum='47' image='/Images/h6.png'/>
          </div>
         <div></div>
